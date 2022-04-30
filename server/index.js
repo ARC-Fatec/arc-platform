b5ecc79f37e5f6beaeb2ed3b6c48a23f4cecab4d
AChris <allan.christian@ibm.com>
2022-04-30 17:06:51 -0300

    Change express instance to prevent code overflow

--- server/index.js
+++ server/index.js
@@ -2,15 +2,19 @@ const express = require('express')
 const app = express()
 
 app.get('/route1', (req, res) => {
-    res.status(200).json({message: 'Successfully reached'})
+    res.json({message: 'Successfully reached'})
 })
 
 app.get('/route2', (req, res) => {
-    res.status(200).json({message: 'Successfully reached'})
+    res.json({message: 'Successfully reached'})
 })
 
 app.get('/route3', (req, res) => {
-    res.status(200).json({message: 'Successfully reached'})
+    res.json({message: 'Successfully reached'})
+})
+
+app.use('/useRoute', (req, res) => {
+    res.json({message: 'Successfully reached'})
 })
 
 app.listen(3000, () => {
