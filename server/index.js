cb40cf687ebec047306a0b99c82273718f72a962
AChris <allan.christian@ibm.com>
2022-04-30 16:20:48 -0300

    First api test

--- server/index.js
+++ server/index.js
@@ -1,2 +1,18 @@
-console.log('Hello world')
-console.log('Hello world')
+const express = require('express')
+const app = express()
+
+app.get('/route1', (req, res) => {
+    res.status(200).json({message: 'Successfully reached'})
+})
+
+app.get('/route2', (req, res) => {
+    res.status(200).json({message: 'Successfully reached'})
+})
+
+app.get('/route3', (req, res) => {
+    res.status(200).json({message: 'Successfully reached'})
+})
+
+app.listen(3000, () => {
+    console.log("Server working fine")
+})
