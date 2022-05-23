c695da94f463955cd90dbea8adc5659678ca792e
allanchristiang <allancgadelha@gmail.com>
2022-05-23 10:55:37 -0300

    Add support views to login and signup

---
+++ src/components/SignIn.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <!--Form-->
+        <div class="wrap">
+          <h1>ARC</h1>
+          <form >
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email">
+            </div>
+            <div class="form-group">
+              <label for="password">Senha</label>
+              <input type="password">
+            </div>
+          </form>
+          <p>Esqueceu a Senha? Clique <a href="#">aqui</a> </p>
+          <p>Precisa acessar <a href="#">siga</a> </p>
+        </div>
+      </div>
+      <button class="btn btn-primary">Sign in</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      text: 'Nice'
+
+    }
+  },
+  method: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import SignUp from './SignUp.vue'
 
-Vue.config.productionTip = false
+// Vue.component('sign-up', SignUp)
+
+// Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
