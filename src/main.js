ae31deea67d6e22353793721750dbd943dcce48e
allanchristiang <allancgadelha@gmail.com>
2022-05-24 12:34:26 -0300

    Add router fallback option

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+// import VueRouter from 'vue-router'
 import App from './App.vue'
 import SignIn from './components/SignIn.vue'
 import SignUp from './components/SignUp.vue'
 
 // Vue.config.productionTip = false
 
-Vue.use(VueRouter)
+// Vue.use(VueRouter)
 
-const router = new VueRouter ({
-  routes: [
-    { path: '/cadastro', component: SignUp }, //teste.com/cadastro
-    { path: '/', component: SignIn }//teste.com
-  ]
-})
+// const router = new VueRouter ({
+//   routes: [
+//     { path: '/cadastro', component: SignUp }, //teste.com/cadastro
+//     { path: '/', component: SignIn }//teste.com
+//   ]
+// })
 
 new Vue({
-  router,
+  // router,
   render: h => h(App)
 }).$mount('#app')
 
